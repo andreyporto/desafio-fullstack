34d067e8e8295dfc66da55fe8f8f99fcb1d7d236
andreyporto <andreybrumatti@gmail.com>
2026-07-10 21:01:06 -0300

    fix: remove comment

--- wavechat-api/app/Http/Resources/Auth/RegisterResource.php
+++ wavechat-api/app/Http/Resources/Auth/RegisterResource.php
@@ -7,19 +7,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RegisterResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
-            'message' => 'Usuário registrado com sucesso',
-            'user' => [
-                'id' => $this->id,
-                'name' => $this->name,
-                'email' => $this->email,
+            "message" => "Usuário registrado com sucesso",
+            "user" => [
+                "id" => $this->id,
+                "name" => $this->name,
+                "email" => $this->email,
             ],
         ];
     }
